8e4faeef73bad474c3543327ce85c749a1575988
Ajay-tungston <you@example.com>
2026-01-17 12:33:51 +0530

    Enabled toast message in createAccount screen

--- App.tsx
+++ App.tsx
@@ -3,12 +3,11 @@ import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 import Navigation from "./src/navigation";
 import Toast from "react-native-toast-message";
-
+import { toastConfig } from "./src/utils/toastConfig";
 export default function App() {
   return (
     <Provider store={store}>
       <Navigation />
-      <Toast />
-    </Provider>
+<Toast config={toastConfig} />    </Provider>
   );
 }
